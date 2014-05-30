a7043ee4647a4340eb459c1138a4550885783727
Guillem Barba <guillem@nan-tic.com>
2014-05-30 19:52:53 +0200

    [#020375] CodeReview state field as normal Selection field with buttons to change its value

--- work.py
+++ work.py
@@ -1,6 +1,5 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-
 from trytond.model import ModelView, ModelSQL, fields
 from trytond.pool import PoolMeta, Pool
 from trytond.pyson import Eval, And
@@ -24,19 +23,57 @@ class ProjectCodeReview(ModelSQL, ModelView):
     component = fields.Many2One('project.work.component', 'Component',
         required=True, select=True)
     comment = fields.Text('comment')
-    state = fields.Function(fields.Char('State'),
-        'get_codereview_state', searcher='search_codereview_state')
-
-    def get_codereview_state(self, name):
-        return self.work.state
+    state = fields.Selection([
+            ('opened', 'Opened'),
+            ('done', 'Done'),
+            ], 'State', required=True, readonly=True, select=True)
 
     @classmethod
-    def search_codereview_state(cls, name, clause):
-        pool = Pool()
-        Work = pool.get('project.work')
-        works = Work.search(clause)
-        work_ids = [x.id for x in works]
-        return [('work', 'in', work_ids)]
+    def __setup__(cls):
+        super(ProjectCodeReview, cls).__setup__()
+        cls._error_messages.update({
+                'invalid_work_state': ('Code Review "%(codereview)s" can not '
+                    'be opened because its work "%(work)s" is already done.'),
+                })
+        cls._buttons.update({
+                'open': {
+                    'invisible': Eval('state') == 'opened',
+                    },
+                'done': {
+                    'invisible': Eval('state') == 'done',
+                    },
+                })
+
+    @staticmethod
+    def default_state():
+        return 'opened'
+
+    @classmethod
+    def validate(cls, codereviews):
+        super(ProjectCodeReview, cls).validate(codereviews)
+        for codereview in codereviews:
+            codereview.check_state()
+
+    def check_state(self):
+        if self.state == 'opened' and self.work.state == 'done':
+            self.raise_user_error('invalid_work_state', {
+                    'codereview': self.rec_name,
+                    'work': self.work.rec_name,
+                    })
+
+    @classmethod
+    @ModelView.button
+    def open(cls, codereviews):
+        for codereview in codereviews:
+            codereview.state = 'opened'
+            codereview.save()
+
+    @classmethod
+    @ModelView.button
+    def done(cls, codereviews):
+        for codereview in codereviews:
+            codereview.state = 'done'
+            codereview.save()
 
 
 class Work:
@@ -45,4 +82,25 @@ class Work:
     codereview = fields.One2Many('project.work.codereview', 'work',
         'Codereviews', states={
             'invisible': Eval('type') != 'task',
-        }, depends=['type'])
+            }, depends=['type'])
+
+    @classmethod
+    def __setup__(cls):
+        super(Work, cls).__setup__()
+        cls._error_messages.update({
+                'invalid_codereview_state': (
+                    'Work "%(work)s" can not be set done because its '
+                    'codereview "%(codereview)s" is still opened.'),
+                })
+
+    @classmethod
+    def write(cls, works, vals):
+        if vals.get('state', '') == 'done':
+            for work in works:
+                for codereview in work.codereview:
+                    if codereview.state == 'opened':
+                        cls.raise_user_error('invalid_codereview_state', {
+                                'codereview': codereview.rec_name,
+                                'work': work.rec_name,
+                                })
+        super(Work, cls).write(works, vals)
