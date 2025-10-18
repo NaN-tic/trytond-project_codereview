bf1104a615699968ef695d89d02bca5ca5ca730a
Albert Cervera i Areny <albert@nan-tic.com>
2025-10-18 20:33:35 +0200

    Update relationship between project.work and component if codereview changes or is removed.

--- work.py
+++ work.py
@@ -82,27 +82,32 @@ class ProjectCodeReview(ModelSQL, ModelView):
 
     @classmethod
     def create(cls, vlist):
-        pool = Pool()
-        Category = pool.get('project.work.component_category')
-        Component = pool.get('project.work.component')
-        Work = pool.get('project.work')
+        Work = Pool().get('project.work')
 
-        vlist = [x.copy() for x in vlist]
-        for values in vlist:
-            task = Work(values.get('work'))
-            component = Component(values.get('component'))
-            if values.get('category'):
-                category = Category(values.get('category'))
-                if not category in task.component_categories:
-                    task.component_categories += (category,)
-                    task.save()
-            if component not in task.components:
-                task.components += (component),
-                if (component.category and
-                        component.category not in task.component_categories):
-                    task.component_categories += (component.category,)
-                task.save()
-        return super(ProjectCodeReview, cls).create(vlist)
+        records = super().create(vlist)
+        works = list(set(r.work for r in records))
+        Work.sync_components_categories(works)
+        return records
+
+    @classmethod
+    def write(cls, *args):
+        Work = Pool().get('project.work')
+
+        records = sum([x for x in args[::2]], [])
+        # As the codereview could change from one task to another, we need to
+        # sync both the old and the new tasks
+        works = list(set(r.work for r in records))
+        super().write(*args)
+        works += list(set(r.work for r in records))
+        Work.sync_components_categories(works)
+
+    @classmethod
+    def delete(cls, records):
+        Work = Pool().get('project.work')
+
+        works = list(set(r.work for r in records))
+        super().delete(records)
+        Work.sync_components_categories(works)
 
 
 class Work(metaclass=PoolMeta):
@@ -112,6 +117,21 @@ class Work(metaclass=PoolMeta):
         'Codereviews', states={
             'invisible': Eval('type') != 'task',
             })
+
+    @classmethod
+    def sync_components_categories(cls, works):
+        for work in works:
+            components = set()
+            categories = set()
+            for codereview in work.codereviews:
+                components.add(codereview.component)
+                if codereview.category:
+                    categories.add(codereview.category)
+                if codereview.component.category:
+                    categories.add(codereview.component.category)
+            work.components = list(components)
+            work.component_categories = list(categories)
+        cls.save(works)
 
     @classmethod
     def validate(cls, works):
